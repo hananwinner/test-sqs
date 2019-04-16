ce60aca8cf7af7f2a423866b3816c3298097d217
Hanan Wiener <hananskillz@gmail.com>
2019-04-16 12:33:00 +0300

    major refactoring

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import time
 
 in_flight_m = None
 consecutive_time_outs = 0
-queues = []
 
 
 def _get_q_config():
@@ -26,90 +25,62 @@ print("qs from config: {}".format(_qs))
 queues = [sqs.get_queue_by_name(QueueName=q) for q in _get_q_config()]
 
 
-def process_message(m):
-    print('processing message..')
+def _parse_attribute(m, attr_name, attr_type='StringValue', default_value=None, _cast=int):
+    _value = default_value
     if m.message_attributes is not None:
-        attempts = m.message_attributes.get('attempts').get('StringValue')
-        print('debug: raw value attemts: {}'.format(attempts))
-        if attempts:
-            attempts = int(attempts)
-        else:
-            attempts = 0
-        print("debug: attempt: {}".format(attempts))
-    print("debug: body: \n{}".format(m.body))
-
-    if m.body == "crash":
-        print('simulating crash')
-        raise OSError("simulating a crash")
-    elif m.body == "timeout":
-        print('simulating time out')
-        raise TimeoutError("message processing timed out")
-    elif m.body == "long":
-        print('simulating long meesgae by 20 sec sleep')
-        time.sleep(20)
+        _attr = m.message_attributes.get(attr_name)
+        if _attr:
+            _attr_value = _attr.get(attr_type)
+            if _attr_value:
+                _attr_value = _cast(_attr_value)
+    return _value
 
 
-def _resend_with_attr(q, m, message_attempts):
-    q.send_message(
-        MessageBody=m.body,
-        MessageAttributes={
-            'attempts': {
-                'StringValue': str(message_attempts),
-                'DataType': 'Number'
-            }
-        }
-    )
+def _try_ack(m):
+    m.delete()
 
 
-def _process_timed_out_message(q, m):
-    result_status = "dled"
-    message_attempts = 0
-    if m.message_attributes is not None:
-        _dict_attempts = m.message_attributes.get('attempts')
-        if _dict_attempts is not None:
-            message_attempts = _dict_attempts.get('StringValue', 0)
-            message_attempts = int(message_attempts)
-    message_attempts += 1
+def ack_message(m):
+    print('ACK message {}'.format(m.message_id))
     try:
-        if message_attempts >= 2:
-            _resend_with_attr(dead_letter, m, message_attempts)
-        else:
-            _resend_with_attr(q, m, message_attempts)
-            result_status = "retried"
-    finally:
-        m.delete()
-    return result_status
+        _try_ack(m)
+    except botocore.exceptions.ClientError as ex:
+        print("failed to ack message. future duplication is possible.\nex:\n{}".format(ex))
 
 
 def nack_message(m):
-    print('nacking message {}'.format(m.message_id))
+    print('NACK message {}'.format(m.message_id))
     try:
         m.change_visibility(VisibilityTimeout=0)
-    except botocore.exceptions.ClientError:
-        pass
+    except botocore.exceptions.ClientError as ex:
+        print('failed to nack message\nbotocore exception:\n {}'.format(ex))
 
 
-def produce_event_message(m, status, _tic=None):
+def log_event(m, status, _tic=None):
+    print('logging event {} for message {}'.format(status, m.message_id))
     if _tic is not None:
         _dur = time.time() - _tic
     else:
         _dur = "unknown"
-    event_q.send_message(
-        MessageBody="message {} {} in {}s\norig body: \n{}".format(m.message_id, status, _dur,  m.body),
-        MessageAttributes={
-            'duration': {
-                'StringValue': str(_dur),
-                'DataType': 'String'
+    try:
+        event_q.send_message(
+            MessageBody="message {} {} in {}s\norig body: \n{}".format(m.message_id, status, _dur,  m.body),
+            MessageAttributes={
+                'duration': {
+                    'StringValue': str(_dur),
+                    'DataType': 'String'
+                }
             }
-        }
-    )
+        )
+    except botocore.exceptions.ClientError as ex:
+        print('failed log to event queue\nbotocore exception:\n {}'.format(ex))
 
 
 def nack_inflight_message():
-    if in_flight_m:
+    if in_flight_m is not None:
         print('nacking inflight message {}'.format(in_flight_m.message_id))
         nack_message(in_flight_m)
-        produce_event_message(in_flight_m, "nacked on TERM")
+        log_event(in_flight_m, "nacked on TERM")
     else:
         print('no inflight messages')
 
@@ -122,6 +93,62 @@ def sigterm_handler(signum, frame):
 signal.signal(signal.SIGTERM, sigterm_handler)
 
 
+def try_process_message(m):
+    print('processing message..')
+
+    print("debug: body: \n{}".format(m.body))
+
+    if m.body == "crash":
+        print('simulating crash')
+        raise OSError("simulating a crash")
+    elif m.body == "timeout":
+        print('simulating time out')
+        raise TimeoutError("message processing timed out")
+    elif m.body == "long":
+        print('simulating long meesgae by 20 sec sleep')
+        time.sleep(20)
+
+    _try_ack(m)
+
+
+def _try_resend_with_attr(q, m, num_attempts):
+    q.send_message(
+        MessageBody=m.body,
+        MessageAttributes={
+            'attempts': {
+                'StringValue': str(num_attempts),
+                'DataType': 'Number'
+            }
+        }
+    )
+
+
+def _process_timed_out_message(q, m, previous_attempts):
+    result_status = "failed to process on timeout"
+    _attempts = previous_attempts + 1
+    if _attempts >= 2:
+        try:
+            print("too many attempts. sending to dead-letter.")
+            _try_resend_with_attr(dead_letter, m, _attempts)
+            result_status = "dead-lettered"
+        except botocore.exceptions.ClientError as ex:
+            print('failed to send to dead letter\nbotocore exception:\n {}'.format(ex))
+            nack_message(m)
+        else:
+            ack_message(m)
+    else:
+        try:
+            print("resending message with increased attempts counter")
+            _try_resend_with_attr(q, m, _attempts)
+            result_status = "retried"
+        except botocore.exceptions.ClientError as ex:
+            print('failed to increase attempts time out attribute by re-sending.\nbotocore exception:\n {}'.format(ex))
+            nack_message(m)
+        else:
+            ack_message(m)
+
+    return result_status
+
 
 def main():
     global in_flight_m
@@ -133,35 +160,26 @@ def main():
                     in_flight_m = m
                     _tic = time.time()
                     _status = ""
+                    m_timeout_previous_attempts = _parse_attribute(m, 'attempts')
+                    _exception = None
                     try:
-                        process_message(m)
-                        in_flight_m = None
-                        _status = "processed"
-                    except TimeoutError as toe:
-                        print(toe)
-                        consecutive_time_outs += 1
-                        _status = _process_timed_out_message(q, m)
-                        in_flight_m = None
-                        produce_event_message(m, _status, _tic)
-                        raise toe
-                    except OSError as ose:  # general engine error
-                        print(ose)
-                        nack_message(m)
-                        in_flight_m = None
-                        _status = "nacked"
-                        produce_event_message(m, _status, _tic)
-                        raise ose
-                    else:
-                        consecutive_time_outs = 0
-                        m.delete()
-                        in_flight_m = None
+                        try_process_message(m)
                         _status = "acked"
-                        produce_event_message(m, _status, _tic)
+                        consecutive_time_outs = 0
+                    except TimeoutError as ex:
+                        consecutive_time_outs += 1
+                        _status = _process_timed_out_message(q, m, m_timeout_previous_attempts)
+                        _exception = ex
+                    except OSError as ex:  # general engine error
+                        nack_message(m)
+                        _status = "nacked"
+                        _exception = ex
 
+                    in_flight_m = None
+                    log_event(m, _status, _tic)
+                    if _exception:
+                        raise _exception
 
-
-        except OSError:
-            pass  # by choice we let it hang till we figure out
         except TimeoutError:
             if consecutive_time_outs > 5:
                 # die
@@ -170,6 +188,10 @@ def main():
             else:
                 print('sleep to recover after {} cons timeout'.format(consecutive_time_outs))
                 time.sleep(consecutive_time_outs)  # let the system recover..
+
+        except OSError as ex:
+            print('exception:\n {}'.format(ex))
+            # by choice we let it hang till we figure out
 
 
 if __name__ == "__main__":
